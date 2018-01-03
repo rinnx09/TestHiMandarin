1c00aac641c85685f92ffae2421ab4f9d62acd8d
RinnxYii <rinnx09@gmail.com>
2018-01-03 21:20:05 +0800

    Layout arhhhh

---
+++ app/src/main/java/com/example/rinnxyii/testhimandarin/LessonsActivity.java
@@ -0,0 +1,48 @@
+package com.example.rinnxyii.testhimandarin;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class LessonsActivity extends AppCompatActivity {
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_lesson:
+                    mTextMessage.setText("Lessons");
+                    return true;
+                case R.id.navigation_quiz:
+                    mTextMessage.setText("Quiz");
+                    return true;
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_profile:
+                    mTextMessage.setText("Profile");
+                    return true;
+                case R.id.navigation_setting:
+                    mTextMessage.setText("Settings");
+                    return true;
+            }
+            return false;
+        }
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lessons);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+    }
+}
